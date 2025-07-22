b86462cf61403d3c7d3f70fb7f534971af2bff7e
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-07-22 14:26:42 +0400

    Person class

--- Program.cs
+++ Program.cs
@@ -4,7 +4,65 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+        } // End of Main method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }// End of Program class
+
+
+
+    // Classes
+
+    // Person class
+    class Person
+    {
+        public int Id;
+        public string Name;
+        public int Age;
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine("Name: " + Name + ", Age: " + Age);
         }
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}// End of namespace HospitalSystemTask_OOP
